ce5b54f07570a3fe0626391a4f93c985ac552cad
Juan Candia <candia96@gmail.com>
2015-04-10 00:24:21 -0300

    segundo commit

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -69,6 +69,6 @@ class ProductosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def producto_params
-      params.require(:producto).permit(:tipo, :precio, :detalle, :img, :codigo)
+      params.require(:producto).permit(:tipo, :precio, :detalle, :img, :codigo, :categoria)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409194329) do
+ActiveRecord::Schema.define(version: 20150410022441) do
 
   create_table "productos", force: true do |t|
     t.string   "tipo"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150409194329) do
     t.string   "codigo"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "categoria"
   end
 
 end
